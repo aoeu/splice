332a1947024144f1e0a072f81501f1e2e766718e
Travis <travis@mcdem.us>
2015-03-06 10:10:28 -0500

    Add an incomplete Header to decoder

--- decoder.go
+++ decoder.go
@@ -13,3 +13,24 @@ func DecodeFile(path string) (*Pattern, error) {
 // drum pattern contained in a .splice file.
 // TODO: implement
 type Pattern struct{}
+
+/*
+Saved with HW Version: 0.808-alpha
+Tempo: 120
+(0) kick	|x---|x---|x---|x---|
+(1) snare	|----|x---|----|x---|
+(2) clap	|----|x-x-|----|----|
+(3) hh-open	|--x-|--x-|x-x-|--x-|
+(4) hh-close	|x---|x---|----|x--x|
+(5) cowbell	|----|----|--x-|----|
+*/
+
+type Header struct {
+	ChunkID         [6]byte  // 0 - 5
+	Padding1        [7]byte  // 6
+	Unknown1        [1]byte  // 13
+	HardwareVersion [31]byte // 14 - 45
+	Unknown2        [5]byte  // 46 - 50
+	Padding3        [3]byte  // 51 - 53 // ??? Is the BPM * 2 in here?
+	Unknown3        [1]byte  // 54
+}
