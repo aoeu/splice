63224a4aede4ccb9f17f32a3e90a8f08be3f33d8
Travis <travis@mcdem.us>
2015-03-07 15:59:57 -0500

    Add incremental improvements, not entirely working

--- decoder.go
+++ decoder.go
@@ -5,7 +5,9 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"regexp"
 )
 
 // DecodeFile decodes the drum machine file found at the provided path
@@ -21,18 +23,50 @@ func DecodeFile(path string) (*Pattern, error) {
 	if binary.Read(f, binary.LittleEndian, &p.Header); err != nil {
 		return p, err
 	}
-	for {
-		drumPart, err := readDrumPart(f)
+	p.HardwareVersion = string(bytes.Trim(p.Header.HardwareVersion[:], "\x00"))
+	reader := io.Reader(f)
+	switch p.HardwareVersion {
+	case "0.808-alpha":
+		p.Tempo = p.Header.BPM / 2
+		if p.Header.BPMDecimal != 0 {
+			// TODO: Is this really the correct way to determine the decimal?
+			p.TempoDecimal = p.Header.BPMDecimal - 200
+		}
+	case "0.909":
+		// TODO: Is there no byte this value can be pulled from?
+		p.Tempo = 240
+	case "0.708-alpha":
+		fmt.Println("uhetnashutnseoah")
+		p.Tempo = 128
+		// TODO: Filter out SPLICE\x00\x00
+		b, err := ioutil.ReadAll(f)
 		if err != nil {
-			if err == io.EOF {
-				return p, nil
-			}
 			return p, err
 		}
-		fmt.Sprintln("%+v %v", drumPart, err)
-		p.DrumParts = append(p.DrumParts, drumPart)
+		spliceRe := regexp.MustCompile("SPLICE\x00\x00\x00") // TODO: MOVE THIS
+		b = spliceRe.ReplaceAllLiteral(b, []byte{})
+		reader = bytes.NewReader(b)
+		fmt.Printf("%s\n", string(b))
 	}
-	return p, nil
+	p.DrumParts, err = readAllDrumParts(reader)
+	return p, err
+}
+
+func readAllDrumParts(r io.Reader) (DrumParts, error) {
+	d := make([]DrumPart, 0)
+	for {
+		drumPart, err := readDrumPart(r)
+		fmt.Printf("%+v : %+v\n", drumPart, err)
+		if err != nil {
+			if err == io.EOF {
+				return d, nil
+			}
+			return d, err
+		}
+		fmt.Sprintln("%+v %v", drumPart, err)
+		d = append(d, drumPart)
+	}
+	return d, nil
 }
 
 type DrumParts []DrumPart
@@ -49,6 +83,9 @@ func (d DrumParts) String() string {
 // drum pattern contained in a .splice file.
 type Pattern struct {
 	Header
+	Tempo           byte
+	TempoDecimal    byte
+	HardwareVersion string
 	DrumParts
 }
 
@@ -63,10 +100,11 @@ var i int = 1
 func (p Pattern) String() string {
 	h := bytes.Trim(p.Header.HardwareVersion[:], "\x00")
 	i++
-	bpm := fmt.Sprintf("%v", (p.Header.BPM / 2))
-	if p.Header.BPMDecimal != 0 {
+	bpm := fmt.Sprint(p.Tempo)
+	fmt.Println(bpm)
+	if p.TempoDecimal != 0 {
 		// TODO: Is this really the correct way to determine the decimal?
-		bpm = fmt.Sprintf("%v.%v", bpm, p.Header.BPMDecimal-200)
+		bpm = fmt.Sprintf("%v.%v", p.Tempo, p.TempoDecimal)
 	}
 	s := fmt.Sprintf("Saved with HW Version: %s\nTempo: %v\n%v", h, bpm, p.DrumParts)
 	return s
@@ -77,9 +115,9 @@ type Header struct {
 	Padding1        [7]byte  // 6
 	Unknown1        [1]byte  // 13
 	HardwareVersion [31]byte // 14 - 45
-	Unknown2	[2]byte
-	BPMDecimal      byte
-	BPM             byte
+	Unknown2        [2]byte
+	BPMDecimal      byte // BPM Decimal for 808
+	BPM             byte // BPM for 808
 	Unknown3        byte
 }
 
