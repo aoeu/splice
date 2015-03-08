2ad188ab7eeb2ce6f8b16766dc6da8d097996219
Travis <travis@mcdem.us>
2015-03-08 01:00:22 -0500

    Add incomplete encoder scaffolding

---
+++ encoder.go
@@ -0,0 +1,7 @@
+package drum
+
+import ()
+
+func Encode(input string) ([]byte, error) {
+	return []byte{}, nil
+}
---
+++ encoder_test.go
@@ -0,0 +1,41 @@
+package drum
+
+import (
+	"io/ioutil"
+	"path"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	tData := []struct {
+		path   string
+		backup string
+	}{
+		{"pattern_1",
+			`Saved with HW Version: 0.808-alpha
+Tempo: 120
+(0) kick	|x---|x---|x---|x---|
+(1) snare	|----|x---|----|x---|
+(2) clap	|----|x-x-|----|----|
+(3) hh-open	|--x-|--x-|x-x-|--x-|
+(4) hh-close	|x---|x---|----|x--x|
+(5) cowbell	|----|----|--x-|----|
+`,
+		},
+	}
+
+	for _, input := range tData {
+		actual, err := Encode(input.backup)
+		if err != nil {
+			t.Fatalf("Something went wrong encoding - %v", err)
+		}
+		filePath := path.Join("patterns", input.path+".splice")
+		expected, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			t.Fatalf("Could not read input file at path %v - %v", filePath, err)
+		}
+		if len(expected) != len(actual) {
+			t.Fatalf("Expected %v output bytes and got %v", len(expected), len(actual))
+		}
+	}
+}
