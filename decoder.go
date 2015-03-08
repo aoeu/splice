f5f1807e881cbe36b96020da3d312118859d8244
Travis <travis@mcdem.us>
2015-03-08 00:47:26 -0500

    Rearrange structs and methods for package

--- decoder.go
+++ decoder.go
@@ -12,23 +12,24 @@ import (
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
 func DecodeFile(path string) (*Pattern, error) {
+	h := header{}
 	p := NewPattern()
 	f, err := os.Open(path)
 	if err != nil {
 		return p, err
 	}
 	defer f.Close()
-	if binary.Read(f, binary.LittleEndian, &p.header); err != nil {
+	if binary.Read(f, binary.LittleEndian, &h); err != nil {
 		return p, err
 	}
-	p.HardwareVersion = string(bytes.Trim(p.header.HardwareVersion[:], "\x00"))
+	p.HardwareVersion = string(bytes.Trim(h.HardwareVersion[:], "\x00"))
 	reader := io.Reader(f)
 	switch p.HardwareVersion {
 	case "0.808-alpha":
-		p.Tempo = int(p.header.Tempo) / 2
-		if p.header.TempoDecimal != 0 {
+		p.Tempo = int(h.Tempo) / 2
+		if h.TempoDecimal != 0 {
 			// TODO(aoeu): Is this really the correct way to determine the decimal?
-			p.TempoDecimal = int(p.header.TempoDecimal) - 200
+			p.TempoDecimal = int(h.TempoDecimal) - 200
 		}
 	case "0.909":
 		// TODO(aoeu): Is there no byte this value can be pulled from?
@@ -68,85 +69,14 @@ func (d Tracks) String() string {
 	return s
 }
 
-// Pattern is the high level representation of the
-// drum pattern contained in a .splice file.
-type Pattern struct {
-	header
-	Tempo           int
-	TempoDecimal    int
-	HardwareVersion string
-	Tracks
-}
-
-// NewPattern returns an empty pattern.
-func NewPattern() *Pattern {
-	// TODO(aoeu): json.NewDecoder(r io.Reader) could be influential.
-	p := new(Pattern)
-	p.Tracks = make([]Track, 0)
-	return p
-}
-
 func (p Pattern) String() string {
-	h := bytes.Trim(p.header.HardwareVersion[:], "\x00")
 	bpm := fmt.Sprint(p.Tempo)
 	if p.TempoDecimal != 0 {
 		// TODO(aoeu): Is this really the correct way to determine the decimal?
 		bpm = fmt.Sprintf("%v.%v", p.Tempo, p.TempoDecimal)
 	}
-	s := fmt.Sprintf("Saved with HW Version: %s\nTempo: %v\n%v", h, bpm, p.Tracks)
+	s := fmt.Sprintf("Saved with HW Version: %s\nTempo: %v\n%v", p.HardwareVersion, bpm, p.Tracks)
 	return s
-}
-
-type header struct {
-	// TODO(aoeu): What are better names for "Unknown.*"?
-	ChunkID         [6]byte  // 0 - 5
-	Padding1        [7]byte  // 6
-	Unknown1        [1]byte  // 13
-	HardwareVersion [31]byte // 14 - 45
-	Unknown2        [2]byte
-	TempoDecimal    byte // Tempo Decimal for 808
-	Tempo           byte // Tempo for 808
-	Unknown3        byte
-}
-
-// A Track represents a named, identified drum sequence.
-type Track struct {
-	ID       byte
-	Name     string
-	Sequence []byte
-}
-
-const (
-	separator string = "|"
-	onBeat    string = "x"
-	offBeat   string = "-"
-	errorRune string = "?"
-)
-
-func (t Track) String() string {
-	s := fmt.Sprintf("(%d) %s\t", t.ID, t.Name)
-	for i := 0; i < len(t.Sequence); i++ {
-		if i%4 == 0 {
-			s += separator
-		}
-		switch t.Sequence[i] {
-		case 1:
-			s += onBeat
-		case 0:
-			s += offBeat
-		default:
-			s += errorRune
-		}
-	}
-	s += separator
-	return s
-}
-
-// NewTrack returns an empty, initialized track.
-func NewTrack() *Track {
-	t := new(Track)
-	t.Sequence = make([]byte, 16)
-	return t
 }
 
 func readTrack(r io.Reader) (Track, error) {
--- drum.go
+++ drum.go
@@ -1,3 +1,80 @@
 // Package drum is supposed to implement the decoding of .splice drum machine files.
 // See golang-challenge.com/go-challenge1/ for more information
 package drum
+
+import (
+	"fmt"
+)
+
+type header struct {
+	// TODO(aoeu): What are better names for "Unknown.*"?
+	ChunkID         [6]byte  // 0 - 5
+	Padding1        [7]byte  // 6
+	Unknown1        [1]byte  // 13
+	HardwareVersion [31]byte // 14 - 45
+	Unknown2        [2]byte
+	TempoDecimal    byte // Tempo Decimal for 808
+	Tempo           byte // Tempo for 808
+	Unknown3        byte
+}
+
+// Pattern is the high level representation of the
+// drum pattern contained in a .splice file.
+type Pattern struct {
+	Tempo           int
+	TempoDecimal    int
+	HardwareVersion string
+	Tracks
+}
+
+// NewPattern returns an empty pattern.
+func NewPattern() *Pattern {
+	// TODO(aoeu): json.NewDecoder(r io.Reader) could be influential.
+	p := new(Pattern)
+	p.Tracks = make([]Track, 0)
+	return p
+}
+
+func NewPatternFromBackup(input string) (Pattern, error) {
+	return Pattern{}, nil
+}
+
+// A Track represents a named, identified drum sequence.
+type Track struct {
+	ID       byte
+	Name     string
+	Sequence []byte
+}
+
+// NewTrack returns an empty, initialized track.
+func NewTrack() *Track {
+	t := new(Track)
+	t.Sequence = make([]byte, 16)
+	return t
+}
+
+const (
+	separator string = "|"
+	onBeat    string = "x"
+	offBeat   string = "-"
+	errorRune string = "?"
+)
+
+func (t Track) String() string {
+	s := fmt.Sprintf("(%d) %s\t", t.ID, t.Name)
+	for i := 0; i < len(t.Sequence); i++ {
+		if i%4 == 0 {
+			s += separator
+		}
+		switch t.Sequence[i] {
+		case 1:
+			s += onBeat
+		case 0:
+			s += offBeat
+		default:
+			s += errorRune
+		}
+	}
+	s += separator
+	return s
+}
